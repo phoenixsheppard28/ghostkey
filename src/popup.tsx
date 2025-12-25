f696990b83f670b3c66a87b77adb958e8f83cc25
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-12-24 23:48:36 -0500

    starting

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,17 +4,21 @@ function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
+    <div style={{ width: "150px" }}>
       <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+        Welcome to Email-llm!
       </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlasmoHQ</footer>
+      <h4>
+        if you're just starting out, check out our {" "}
+        <a
+          href="chrome-extension://hcoeajiofhoifjklbclambdgnahafihp/tabs/quickstart.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          quickstart guide!
+        </a>
+      </h4>
+      <p></p>
     </div>
   )
 }
---
+++ src/tabs/quickstart.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+import "@/style.css"
+
+function IndexNewtab() {
+
+  return (
+    <div className="new-tab">
+    </div>
+  )
+}
+
+export default IndexNewtab
